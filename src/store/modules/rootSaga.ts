cc14990cdc24dd2acb3cad14ead0e072c7d5eff2
Luiz Henrique <7henrique18@gmail.com>
2022-01-05 15:49:40 -0300

    :sparkles: Configure redux-saga

--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -2,15 +2,6 @@ import { all } from "redux-saga/effects";
 
 import cart from "./cart/sagas";
 
-export interface ResponseGenerator {
-  config?: any;
-  data?: any;
-  headers?: any;
-  request?: any;
-  status?: number;
-  statusText?: string;
-}
-
 export default function* rootSaga() {
   const response: unknown = yield all([cart]);
 
