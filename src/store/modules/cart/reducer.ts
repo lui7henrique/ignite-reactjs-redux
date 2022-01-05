6167ce2166f903735f35257364554b00320dd4b0
Luiz Henrique <7henrique18@gmail.com>
2022-01-05 14:19:57 -0300

    :sparkles: Changing state

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,7 +5,19 @@ const INITIAL_STATE: ICartState = {
   items: [],
 };
 
-export const cart: Reducer<ICartState> = (state, action) => {
-  console.log(state, action);
-  return INITIAL_STATE;
+export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "ADD_PRODUCT_TO_CART": {
+      const { product } = action.payload;
+
+      return {
+        ...state,
+        items: [...state.items, { ...product, quantity: 1 }],
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 };
